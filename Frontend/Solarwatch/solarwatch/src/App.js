55af112dc08142c846caf11f3dd18808bf900e9d
Lali95 <tothlajos.m@gmail.com>
2024-01-07 19:19:58 +0100

    feat: add landingpage and registration

---
+++ Frontend/Solarwatch/solarwatch/src/App.js
@@ -0,0 +1,57 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import './index.css'
+
+
+import Layout from './Pages/Layout';
+import ErrorPage from "./Pages/ErrorPage";
+import Products from './Pages/Products';
+import Registration from './Pages/Registration';
+import Login from './Pages/Login';
+import Profile from './Pages/Profile';
+import LandingPage from './Pages/LandingPage';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    errorElement: <ErrorPage />,
+    children: [
+       {
+        path: "/",
+        element: <LandingPage />,
+      },
+   
+     /*  {
+        path: "/:movieId/products",
+        element: <ProductsByMovie />,
+      },
+      {
+        path: "/:movieId/products/:filter",
+        element: <ProductsByCategories />,
+      },
+      {
+        path: "/:itemId",
+        element: <Item />,
+      },
+      {
+        path: "/:userId/cart",
+        element: <Cart />,
+      },
+      {
+        path: "/signin",
+        element: <SignIn />,
+      },
+      */
+    ],
+  },
+]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
