b0278f828fef04b76d11627b1c05977e28421c35
Lali95 <tothlajos.m@gmail.com>
2024-01-07 15:10:01 +0100

    refactor: update City class to have ID for primary key

--- SolarWatch/Data/City.cs
+++ SolarWatch/Data/City.cs
@@ -1,20 +1,24 @@
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace SolarWatch.Models
+namespace SolarWatch.Data
 {
     public class City
     {
         public int Id { get; set; } // Primary key
 
         public string Name { get; set; }
-        
-        [NotMapped] // This tells Entity Framework to ignore Coordinates as an entity
+
+        [NotMapped]
         public Coordinates Coordinates { get; set; }
-        
+
         public string State { get; set; }
         public string Country { get; set; }
+
+        // Navigation property to represent the relationship with SunriseSunset
+        public List<SunriseSunset.SunriseSunset> SunriseSunsets { get; set; }
     }
 
+    // Ensure that Coordinates is in the same namespace
     public class Coordinates
     {
         public double Longitude { get; set; }
--- SolarWatch/Data/WeatherApiContext.cs
+++ SolarWatch/Data/WeatherApiContext.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using SolarWatch.Data;
 using SolarWatch.Models;
 
 public class WeatherApiContext : DbContext
